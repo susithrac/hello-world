ea14caaf48c60eb0f957ceed2734992e5ac08e13
susithrac <susithra_r@yahoo.com>
2020-05-27 17:09:57 +0530

    Update foo.js

--- foo.js
+++ foo.js
@@ -1,3 +1,10 @@
 module.exports = function () {
     console.log('a function in file foo');
 };
+
+<div class="form-group" data-ng-repeat="choice in choices">
+        <label for="choice" ng-show="showChoiceLabel(choice)">Choices</label>
+        <button ng-show="showAddChoice(choice)" ng-click="addNewChoice()">Add Choice</button>
+        <button ng-click="removeNewChoice()">Remove Choice</button>
+        <input type="text" ng-modal="{{choice.name}}" name="" placeholder="Enter a restaurant name" value="{{choice.id}}">
+      </div>
